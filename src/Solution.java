e7b848b8b585fea4ba530d83041e58e830c3ef94
Vladimir Papin <vladimir.papin@gmail.com>
2016-07-28 23:01:57 +0300

    Done task 2058

--- src/Solution.java
+++ src/Solution.java
@@ -1,39 +1,24 @@
+import java.util.HashSet;
 import java.util.Scanner;
-import java.util.TreeMap;
+import java.util.Set;
 
 public class Solution {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        TreeMap<Integer, Integer> map = new TreeMap<>();
 
-        int countLine = scanner.nextInt();
-        for (int i = 0; i < countLine; i++) {
-            int query = scanner.nextInt();
-            if (query == 1) {
-                int valueAsKey = scanner.nextInt();
-                Integer countValue = map.get(valueAsKey);
-                if (countValue == null) {
-                    countValue = 1;
-                } else {
-                    countValue++;
-                }
-                map.put(valueAsKey, countValue);
-            } else {
-                if (map.size() > 0) {
-                    int firstKey = map.firstKey();
-                    int firstValue = map.get(firstKey);
-                    if (firstValue == 1) {
-                        map.remove(firstKey);
-                    } else {
-                        map.put(firstKey, --firstValue);
-                    }
-                    System.out.println(firstKey);
+        String line = scanner.nextLine();
+        Set<String> set = new HashSet<>();
+
+        for (int i = 0; i < line.length(); i++) {
+            for (int j = (line.length()); j >= i; j--) {
+                String temp = line.substring(i, j);
+                if (!temp.isEmpty()) {
+                    set.add(temp);
                 }
             }
         }
-
-
+        System.out.println(set.size());
         scanner.close();
     }
 
