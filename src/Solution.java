ad8290419efb0fa8d1367ac07b6b8fbcae61d3ad
Vladimir Papin <vladimir.papin@gmail.com>
2016-07-29 03:03:35 +0300

    Done task 2053

--- src/Solution.java
+++ src/Solution.java
@@ -1,42 +1,26 @@
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class Solution {
 
     public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
 
-        String[] lineInteger = scanner.nextLine().split(" ");
-//        int n = Integer.valueOf(lineInteger[0]);
-        int k = Integer.valueOf(lineInteger[1]);
+        HashMap<String, Integer> coder = new HashMap<>();
+        int coderNumber = 1;
 
-        String[] array = scanner.nextLine().split(" ");
-        String[] b = scanner.nextLine().split(" ");
-        List<Integer> list = new ArrayList<>();
-        for (int i = 0; i < b.length; i++) {
-            list.add(Integer.valueOf(b[i]));
-        }
-        for (int i = 0; i < k; i++) {
-            List<String> stringsList = new ArrayList<>();
-            for (int j = 0; j < array.length; j++) {
-                if ((j + 1) % list.get(i) != 0) {
-                    stringsList.add(array[j]);
-                }
+        do {
+            String nextword = scanner.next().toLowerCase();
+            if (coder.get(nextword) == null) {
+                coder.put(nextword, coderNumber);
+                System.out.print(coderNumber + " ");
+                coderNumber++;
+            } else {
+                System.out.print(coder.get(nextword) + " ");
             }
-            array = stringsList.toArray(new String[stringsList.size()]);
-        }
-        for (int i = 0; i < array.length; i++) {
-            System.out.print(array[i]);
-            if ((i + 1) != array.length) {
-                System.out.print(" ");
-            }
-        }
-        System.out.println("");
+        } while (scanner.hasNext());
 
-        scanner.close();
     }
 
 }
-
